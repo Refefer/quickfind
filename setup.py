9332004fa0b9a27821403afc56dc77a9a54051e6
Andrew Stanton <Refefer@gmail.com>
2014-03-25 20:15:54 -0700

    Moves, adds install file

---
+++ setup.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='quickfind',
+      version="0.1.0.0",
+      description='Fuzzy find for the terminal',
+      url="https://github.com/Refefer/quickfind",
+      packages=['quickfind'],
+      license="LICENSE",
+      scripts=['bin/qf'],
+      author='Andrew Stanton',
+      author_email='Andrew Stanton',
+      install_requires=['python-ctags'],
+      classifiers=[
+       "License :: OSI Approved :: Apache Software License",
+       "Programming Language :: Python :: 2.6",
+       "Operating System :: OS Independent"
+      ])  
