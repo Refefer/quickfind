3593d0c26f771f56e261f2fbbe387d154569ea2f
Andrew Stanton <Refefer@gmail.com>
2014-03-25 23:29:32 -0700

    Ups version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='quickfind',
-      version="0.1.0.0",
+      version="0.1.0.1",
       description='Fuzzy find for the terminal',
       url="https://github.com/Refefer/quickfind",
       packages=['quickfind', 'quickfind/source'],
